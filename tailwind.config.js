07f39d1ed237dda2b517862b687cf4fcb383fe76
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-05 20:58:34 +0800

    added custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,13 +22,17 @@ module.exports = {
 
         'project-bg-dark': 'hsla(0, 0%, 16%, .50)',
         'project-bg-light': 'hsla(0, 0%, 100%, .50);',
-      },
 
+        'work-hover': '0px 5px 5px -3px rgba(0, 0, 0, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px white',
+      },
       backgroundImage: {
         'light-image': "radial-gradient(at 24% 90%, hsl(66deg 21% 20% / 42%) 0px, transparent 50%), radial-gradient(at 70% 99%, hsl(175deg 76% 20% / 49%) 0px, transparent 50%)",
-        'dark-image': "radial-gradient(at 24% 90%, hsl(66deg 21% 20% / 75%) 0px, transparent 50%), radial-gradient(at 70% 99%, rgb(1, 82, 75) 0px, transparent 50%)"
-      }
-
+        'dark-image': "radial-gradient(at 24% 90%, hsl(66deg 21% 20% / 75%) 0px, transparent 50%), radial-gradient(at 70% 99%, rgb(1, 82, 75) 0px, transparent 50%)",
+      },
+      boxShadow: {
+        'work-hover-light': '0px 5px 5px -3px rgba(0, 0, 0, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px #06b6d4',
+        'work-hover-dark': '0px 5px 5px -3px rgba(0, 0, 0, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px rgb(55 65 81 / 1)', 
+      },
     },
   },
   plugins: [],
