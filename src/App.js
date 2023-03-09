8280209e80ac93223ab75ef7626f9b73fc3da09e
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-09 19:09:23 +0800

    mod topbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './Home'
 import About from './About'
 import Work from './Work'
-import CV from './assets/NAB-resume.pdf'
+
 
 
 function App() {
@@ -14,23 +14,19 @@ function App() {
   return (
     <Router>
       <div className={darkMode ? "dark" : ""}  >
-        <main className="bg-light bg-light-image px-4 dark:bg-dark  dark:bg-dark-image">
-          <section className="min-h-screen flex flex-col justify-center">
-            <header className="bg-nav-dark dark:bg-nav-light backdrop-blur fixed inset-0 h-16 w-full px-8 flex items-center   justify-between dark:text-white">
-              <Link to='/' className="cursor-pointer">
-                <img className="dark:invert" src={Logo} width="60px" />
-              </Link>
-              <ul className="flex items-center">
-                <li>
-                  <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=" cursor-pointer text-2xl" />
-                </li>
-                <li>
-                  <a className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8" href={CV} download>
-                    Resume
-                  </a>
-                </li>
-              </ul>
-            </header>
+        <main className="bg-light bg-light-image dark:bg-dark  dark:bg-dark-image">
+          <header className="bg-nav-dark dark:bg-nav-light backdrop-blur fixed inset-0 h-16 w-full px-8 flex items-center justify-between dark:text-white z-10">
+            <Link to='/' className="cursor-pointer">
+              <img className="dark:invert" src={Logo} width="60px" />
+            </Link>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=" cursor-pointer text-2xl" />
+              </li>
+            </ul>
+          </header>
+          <section className="min-h-screen flex flex-col justify-center px-4 ">
+
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
