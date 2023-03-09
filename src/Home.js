30815821c6cc93cd0e578d8412b6557592d8ae33
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-10 00:39:04 +0800

    change logo

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,11 @@
 
-import {
-  AiFillGithub,
-  AiFillLinkedin,
-  AiFillMediumSquare,
-} from "react-icons/ai";
+import React from "react"
 
 function Home() {
   const myName = "Neil Adrian Balmes"
   const Profession = "Front-end Developer"
   return (
-    <div className="text-center p-5 "> 
+    <div className="text-center p-5 select-none"> 
       <h2 className="text-3xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl uppercase">
        {myName}
       </h2>
@@ -19,17 +15,6 @@ function Home() {
       <p className="text-sm py-5 leading-5 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
         Developer providing services for  building (and occasionally designing) exceptional digital experiences.
       </p>
-      <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-        <a href="https://github.com/nabalmes" target="_blank">
-          <AiFillGithub />
-        </a>
-        <a href="https://www.linkedin.com/in/adrian-balmes-64990a258/" target="_blank">
-          <AiFillLinkedin />
-        </a>
-        <a href="https://medium.com/@neilbalmes4200" target="_blank">
-          <AiFillMediumSquare/>
-        </a>
-      </div>
     </div>
   )
 }
