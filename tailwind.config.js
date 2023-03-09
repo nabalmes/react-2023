661366bd538376d519d638d170a152118f5273e0
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-09 19:08:16 +0800

    added custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
         // Complex site-specific column configuration
         'sm': '640px',
 
+        //About page custom tw 
+        'about-grid': 'repeat(3, 1fr)',
+
+        //work page custom tw 
         'work-grid-sm': 'repeat(auto-fit, minmax(400px, 1fr))',
         'work-grid': 'repeat(auto-fit, minmax(256px, 1fr))',
 
-        
+
       },
       colors: {
         'light': 'hsla(0,0%,87%,1)',
@@ -20,8 +24,9 @@ module.exports = {
         'nav-light': 'hsla(0, 0%, 13%, .75)',
         'nav-dark': 'hsla(0, 0%, 87%, .75)',
 
-        'project-bg-dark': 'hsla(0, 0%, 16%, .50)',
-        'project-bg-light': 'hsla(0, 0%, 100%, .50);',
+        'card-bg-dark': 'hsla(0, 0%, 16%, .50)',
+        'card-bg-light': 'hsla(0, 0%, 100%, .50);',
+
 
         'work-hover': '0px 5px 5px -3px rgba(0, 0, 0, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px white',
       },
@@ -31,7 +36,7 @@ module.exports = {
       },
       boxShadow: {
         'work-hover-light': '0px 5px 5px -3px rgba(0, 0, 0, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px #06b6d4',
-        'work-hover-dark': '0px 5px 5px -3px rgba(0, 1, 1, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px rgb(55 65 81 / 1)', 
+        'work-hover-dark': '0px 5px 5px -3px rgba(0, 1, 1, .1), 0px 8px 8px 1px rgba(0, 0, 0, .07), 0px 3px 8px 2px rgba(0, 0, 0, .08), 0px 0px 0px 2px rgb(55 65 81 / 1)',
       },
     },
   },
